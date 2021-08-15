bfcdc9b95dde0000185fd29c1aaab2f8928febcc
Joseph <josephiusx@gmail.com>
2021-08-14 21:30:59 -0700

    Bookmark rendered when clicked

--- sec18/src/js/views/recipeView.js
+++ sec18/src/js/views/recipeView.js
@@ -81,11 +81,12 @@ class RecipeView extends View {
             </div>
 
             <div class="recipe__user-generated">
-            
             </div>
             <button class="btn--round btn--bookmark">
             <svg class="">
-                <use href="${icons}#icon-bookmark-fill"></use>
+                <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
             </svg>
             </button>
         </div>
